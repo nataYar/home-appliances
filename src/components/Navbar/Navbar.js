44da7c8c24d707c7f5719d30aede3535e7b6617b
nataliaYarr <n.yarysheva@gmail.com>
2022-08-24 21:18:08 -0700

    home and nav sass applied

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import './Navbar.css';
+import { FaPhoneAlt, FaBars, FaTimes } from 'react-icons/fa';
+import './Navbar.scss';
 
 export default function Navbar( ) {
 
@@ -17,35 +17,53 @@ export default function Navbar( ) {
   const showNavBar = () => {
     navRef.current.classList.toggle('responsive-nav')
     }
+
+  const hideNavBar = () => {
+    navRef.current.classList.remove('responsive-nav')
+  }
   
   return (
     <div className='nav-header'>
-      <h3>Logo</h3>
+      <h3>LOGO</h3>
+      
+      <div className='phone-container'>
+        <div>
+          <a className='phone-icon' href="tel:+15625336324">
+            <FaPhoneAlt />
+          </a>
+        </div>
+        <h3>
+          <a href="tel:+19292977775">(929)297-77-75</a>
+        </h3>
+      </div>
+      
+
       <nav ref={navRef} className='navbar'>
         <NavLink
-        onClick={() => showNavBar()}
+        onClick={() => hideNavBar()}
         to="/"
         className={"nav-link"}
         >home</NavLink>
 
         <NavLink
-        onClick={() => showNavBar()}
+         onClick={() => hideNavBar()}
         to="/services"
         className={"nav-link"}
         >services</NavLink>
 
         <NavLink
-        onClick={() => showNavBar()}
+         onClick={() => hideNavBar()}
         to="/blog"
         className={"nav-link"}
         >blog</NavLink>
 
         <NavLink 
-        onClick={() => showNavBar()}
+         onClick={() => hideNavBar()}
         to="/testimonials"
         className={"nav-link"}
         >testimonials</NavLink>
-        <button className='nav-btn nav-close-btn' onClick={() => showNavBar()}>
+
+        <button id='nav-close-btn' className='nav-btn' onClick={() => showNavBar()}>
           <FaTimes/>
         </button>
       </nav>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Home.scss';
+import { default as mask } from './bagdes/mask.svg';
+import { default as gloves } from './bagdes/gloves.svg';
 
 export default function Home() {
   const navigate = useNavigate()
   return (
-    <div>Home
-      <button onClick={() => navigate('/services')}>See our services</button>
-    </div>
+    <section className='home-container'>
+      <div className='home-title'>
+        <div>
+          <h1>CITY TECH</h1>
+          <h1>HVAC & Appliance Repair</h1>
+        </div>
+        <button id='schedule-call-btn'>Schedule a call</button>
+      </div>
+      <div className='badges-section'>
+        <div className='badges'>
+          <div>
+            <img className='badge-about-us' 
+            src={require('./bagdes/vaccine.png')}
+            />
+          </div>
+          <div>
+            <img className='badge-about-us mask' 
+              src={mask} 
+              />
+          </div>
+          <div>
+            <img className='badge-about-us shoecover' 
+            src={require('./bagdes/shoecover.png')} 
+            />
+          </div>
+          <div>
+            <img className='badge-about-us gloves'
+            src={gloves} 
+            />
+          </div>
+        </div>
+        <h6>All the technicians work wearing gloves, shoe covers and face masks to protects you from COVID-19</h6>  
+      </div>
+      <button className='services-btn' onClick={() => navigate('/services')}>See our services</button>
+    </section>
   )
 }
