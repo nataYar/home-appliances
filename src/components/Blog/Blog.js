d9ea56caf08fcb526e92d7fce9446e522d848073
nataliaYarr <n.yarysheva@gmail.com>
2023-01-04 23:52:15 -0800

    modal-form done

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { db } from '../../firebaseConfig';
 import { collection, onSnapshot } from "firebase/firestore";
+import { FaTimes } from 'react-icons/fa';
 import { Editor, EditorState, convertFromRaw } from 'draft-js';
 import './Blog.scss';
 
@@ -29,23 +30,6 @@ export default function Blog( numOfArticles  ) {
     setSixArticles(articles.slice(0, 5))
   }, [articles])
 
-  // useEffect(() => {
-  //     const displayArticles = onSnapshot(
-  //     collection(db, "blog"), 
-  //     (snapshot) => {
-  //       setArticles(snapshot.docs.map(doc => ({...doc.data(), id: doc.id })))
-  //     })
-    
-  //     // Get metadata properties
-  //     const imgRef = ref(storage, '1.jpg');
-  //     getMetadata(imgRef)
-  //     .then((metadata) => {
-  //       // console.log(metadata)
-  //       // Metadata now contains the metadata for 'images/forest.jpg'
-  //     })
-  //     .catch((error) => console.log(error))
-  //   }, [])
-
   const openModal = (el) => {
     setShowModal(true)
     // displayPost(el)
@@ -55,58 +39,28 @@ export default function Blog( numOfArticles  ) {
     setModal(el)
   }
 
-  // useEffect(()=> {
-  //   displayPost(el)
-  // }, [showModal])
 
   const hideModal = () => {
     setShowModal(!showModal)
   }
-
-  // const ModalContainer = (el, editorState ) => {
-  //   return (
-  //     <div className='modal_container' onClick={() => setShowModal(!showModal)} >
-  //       <div className='modal-guts'>
-  //         <button onClick={hideModal}> close </button>
-  //         {
-  //           el.brand ? <p className=''>{el.brand}</p> : null
-  //         }
-  //         <p className=''>{el.type}</p>
-  //       </div>
-  //       <h2>{el.title}</h2>
-  //       <div className=''>
-  //         <img src={el.imgSrc} alt=''
-  //         />
-  //       </div>
-  //       {/* <Editor editorState={editorState} readOnly={true} /> */}
-  //       <p className='blog-page_date'>{el.time}</p>
-  //     </div>
-  //   )
-  // }
-
-  // const displayPost = (el) => {
-  //   // console.log(el)
-  //   // const contentState = convertFromRaw(el.mainText);
-  //   // const editorState = EditorState.createWithContent(contentState);
-  //    return <ModalContainer el={el} />
-  //   //  editorState={editorState}
-     
-  // }
-
 
   return (
     <div className='blog-list__container'>
       {
         showModal ?
         <div className='modal_container' onClick={() => setShowModal(false)} >
-            <button onClick={hideModal}> close </button>
+            <div className='modal-top'>
+            <FaTimes className='modal-btn' onClick={hideModal}/>
+            {/* <button > close </button> */}
             {
-              modal.brand ? <p className=''>{modal.brand}</p> : null
+              modal.brand ? <p>{modal.brand}</p> : null
             }
-            <p className=''>{modal.type}</p>
+            <p>{modal.type}</p>
             
+            </div>
+          
             <h2>{modal.title}</h2>
-            <div className='blog-list__img'>
+            <div className='modal_img'>
               <img src={modal.imgSrc} alt=''
               />
             </div>
